28969c78528543911cacca07557b8c7567d4047c
JohnArtis <Johnartisiii@gmail.com>
2020-10-19 10:30:47 -0400

    Making a search bar

--- WindowCommands/search.py
+++ WindowCommands/search.py
@@ -1,9 +1,10 @@
 import tkinter as tk
-
+import WindowCommands.settings as st
 class search(object):
     def __init__(self, stockName, userSearch):
         self.stockName = stockName
         self.userSearch = userSearch
+        searchArray = search[1000]
 
     def getStockName(self):
         return self.__stockName
@@ -19,14 +20,24 @@ class search(object):
 
     def parsingStock(self):
         #still need to output the found stock to the News Frame/Canvas
+        filteredSearch = search[100]
+        j = 0
         for i in self.userSearch:
             if(i == str.find("AUD") or i == str.find("CAD") or i == str.find("EUR") or i == str.find("USD") or i == str.find("NZD") or i == str.find("GBP")):
-                return 1
+                print ("hello")
             else:
-                return 0
+                print ("Hekki")
+            
 
 
 
 def Search(window):
-    #Takes in user input and includes searchbar
-    print("hello")
+    searchFrame = tk.Frame(window)
+    tk.Label(searchFrame, text = "Search: ").pack(side = tk.LEFT)
+    edit = tk.Entry(searchFrame)
+    edit.pack(side = tk.LEFT, fill = tk.BOTH, expand =1)
+    edit.focus_set()
+
+    searchButton = tk.Button(searchFrame, text = "Find",  )
+    searchButton.pack(side = tk.RIGHT)
+    searchFrame.pack(side = tk.TOP)
