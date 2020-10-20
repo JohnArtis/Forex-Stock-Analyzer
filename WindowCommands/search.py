0fd62a4f7d335a009f9c91fd62bfd6f89db9f221
JohnArtis <Johnartisiii@gmail.com>
2020-10-20 01:14:58 -0400

    Formatted the scroll bar and quick search

--- WindowCommands/search.py
+++ WindowCommands/search.py
@@ -36,35 +36,37 @@ def Search(window):
 
     #Search Bar which will take in user input
     searchFrame = tk.Frame(window)
-    tk.Label(searchFrame, text = "Search: ").pack(side = tk.LEFT)
-    edit = tk.Entry(searchFrame)
+    tk.Label(searchFrame).pack(side = tk.LEFT)
+    edit = tk.Entry(searchFrame, width = int(.10 * uSet.getWidth()))
     edit.pack(side = tk.LEFT, fill = tk.BOTH, expand =1)
     edit.focus_set()
-    searchButton = tk.Button(searchFrame, text = "Find",  )
+    searchButton = tk.Button(searchFrame, text = "Search",  )
     searchButton.pack(side = tk.RIGHT)
 
     searchFrame.pack(side = tk.TOP)
 
     #Checkboxes for quick searches such as AUD, USD, AAPL
     searchBoxes = tk.Frame(window)
-    AUD = tk.Checkbutton(searchBoxes, text = "AUD", bd = 4)
+    AUD = tk.Checkbutton(searchBoxes, text = "AUD", bd = 4, width = 5)
     AUD.pack(side = tk.LEFT)
-    CAD = tk.Checkbutton(searchBoxes, text = "CAD", bd = 4)
+    CAD = tk.Checkbutton(searchBoxes, text = "CAD", bd = 4, width = 5)
     CAD.pack(side = tk.LEFT)
-    EUR = tk.Checkbutton(searchBoxes, text = "EUR", bd = 4)
+    EUR = tk.Checkbutton(searchBoxes, text = "EUR", bd = 4, width = 5)
     EUR.pack(side = tk.LEFT)
-    GBP = tk.Checkbutton(searchBoxes, text = "GBP", bd = 4)
+    GBP = tk.Checkbutton(searchBoxes, text = "GBP", bd = 4, width = 5)
     GBP.pack(side = tk.LEFT)
-    NZD = tk.Checkbutton(searchBoxes, text = "NZD", bd = 4)
+    NZD = tk.Checkbutton(searchBoxes, text = "NZD", bd = 4, width = 5)
     NZD.pack(side = tk.LEFT)
-    JPY = tk.Checkbutton(searchBoxes, text = "JPY", bd = 4)
+    JPY = tk.Checkbutton(searchBoxes, text = "JPY", bd = 4, width = 5)
     JPY.pack(side = tk.LEFT)
-    USD = tk.Checkbutton(searchBoxes, text = "USD", bd = 4)
+    USD = tk.Checkbutton(searchBoxes, text = "USD", bd = 4, width = 5)
     USD.pack(side = tk.LEFT)
     
     searchBoxes.pack(side = tk.TOP)
 
-    OutputFrame = tk.Frame(window, bg = "yellow", height = int(.3 * uSet.getHeight()), width = int(.98 * uSet.getWidth()))
-
-    OutputFrame.pack(side = tk.BOTTOM)
+    scrollbar = tk.Scrollbar(window)
+    scrollbar.pack(side = tk.RIGHT, fill = tk.Y)
+    OutputFrame = tk.Frame(window, bg = "yellow", height = int(.25 * uSet.getHeight()), width = int(.75 * uSet.getWidth()))
+    
+    OutputFrame.pack()
    
