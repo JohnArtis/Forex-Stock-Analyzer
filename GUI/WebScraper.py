5d39f8010aa68a05b5109bc9a18c911fda25d255
JohnArtis <Johnartisiii@gmail.com>
2020-10-21 23:18:58 -0400

    uses userInput to parse yahoo news

--- GUI/WebScraper.py
+++ GUI/WebScraper.py
@@ -1,2 +1,14 @@
-import urllib*
+import urllib.request as rq
+from urllib import *
+def webParser(x):
+    
+    URL = "https://news.search.yahoo.com/"
+
+    values = {x : "currencies"}
+    data = parse.urlencode(values)
+    data = data.encode('utf-8') # data should be bytes
+    req = request.Request(URL, data)
+    resp = request.urlopen(req)
+    respData = resp.read()
+    print(respData)
 
